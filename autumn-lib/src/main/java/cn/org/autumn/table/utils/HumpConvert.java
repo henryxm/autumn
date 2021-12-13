d7b97aa8225307eea8980e88dd0687eeff0113b7
mac <test>
2021-12-14 02:14:09 +0800

    fix first underline

--- autumn-lib/src/main/java/cn/org/autumn/table/utils/HumpConvert.java
+++ autumn-lib/src/main/java/cn/org/autumn/table/utils/HumpConvert.java
@@ -37,7 +37,7 @@ public class HumpConvert {
     public static String HumpToUnderline(String para) {
         StringBuilder sb = new StringBuilder(para);
         int temp = 0;//定位
-        for (int i = 0; i < para.length(); i++) {
+        for (int i = 1; i < para.length(); i++) {
             if (Character.isUpperCase(para.charAt(i))) {
                 sb.insert(i + temp, "_");
                 temp += 1;
