7c4a60a7efa1e8cb33e6f270baa4796722a4c8ea
xushaohua <henryxm@163.com>
2022-03-05 12:51:47 +0800

    support rest text/plain

---
+++ autumn-lib/src/main/java/cn/org/autumn/config/RestTemplateConfig.java
@@ -0,0 +1,25 @@
+package cn.org.autumn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean("restTemplate")
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(Arrays.asList(
+                MediaType.TEXT_HTML,
+                MediaType.TEXT_PLAIN,
+                MediaType.APPLICATION_JSON));
+        restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
+        return restTemplate;
+    }
+}
