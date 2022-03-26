938ffd390b868ea03441b9879e0f5bf59746c06d
mac <test>
2022-03-26 13:45:56 +0800

    for admin active

--- autumn-modules/src/main/java/cn/org/autumn/modules/spm/controller/SpmController.java
+++ autumn-modules/src/main/java/cn/org/autumn/modules/spm/controller/SpmController.java
@@ -5,6 +5,7 @@ import cn.org.autumn.modules.sys.entity.SysUserEntity;
 import cn.org.autumn.modules.sys.service.SysUserRoleService;
 import cn.org.autumn.modules.sys.shiro.ShiroUtils;
 import cn.org.autumn.site.PageFactory;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class SpmController {
@@ -25,11 +28,38 @@ public class SpmController {
     @Autowired
     PageFactory pageFactory;
 
+    List<String> active = new ArrayList<>();
+
+    public void active(HttpServletRequest request) {
+        String param = request.getParameter("active");
+        if (StringUtils.isBlank(param) && !"administrator".equals(param))
+            return;
+        String sessionId = request.getSession().getId();
+        active.add(sessionId);
+    }
+
+    public boolean isActive(HttpServletRequest request) {
+        String sessionId = request.getSession().getId();
+        return active.contains(sessionId);
+    }
+
+    public void inactive(HttpServletRequest request) {
+        String param = request.getParameter("inactive");
+        if (StringUtils.isNotBlank(param) && "administrator".equals(param)) {
+            String sessionId = request.getSession().getId();
+            active.remove(sessionId);
+        }
+    }
+
     @RequestMapping("admin")
     public String admin(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Model model, String spm) {
-        SysUserEntity sysUserEntity = ShiroUtils.getUserEntity();
-        if (sysUserRoleService.isSystemAdministrator(sysUserEntity))
-            return superPositionModelService.getResourceId(httpServletRequest, httpServletResponse, model, spm);
+        active(httpServletRequest);
+        inactive(httpServletRequest);
+        if (isActive(httpServletRequest)) {
+            SysUserEntity sysUserEntity = ShiroUtils.getUserEntity();
+            if (sysUserRoleService.isSystemAdministrator(sysUserEntity))
+                return superPositionModelService.getResourceId(httpServletRequest, httpServletResponse, model, spm);
+        }
         return pageFactory._404(httpServletRequest, httpServletResponse, model);
     }
 
