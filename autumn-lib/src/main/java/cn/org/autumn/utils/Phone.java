35e14d4aac7278414df26c87eec6230f37045671
mac <test>
2022-10-21 23:00:55 +0800

    fix empty phone

--- autumn-lib/src/main/java/cn/org/autumn/utils/Phone.java
+++ autumn-lib/src/main/java/cn/org/autumn/utils/Phone.java
@@ -1,5 +1,7 @@
 package cn.org.autumn.utils;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +13,8 @@ public class Phone {
     }
 
     public static boolean isPhone(String phone) {
+        if (StringUtils.isBlank(phone))
+            return false;
         String regex = "^((13[0-9])|(14[0-9])|(15([0-9]))|(16[0-9])|(17[0-9])|(18[0-9])|(19[0-9]))\\d{8}$";
         if (phone.length() != 11) {
             return false;
