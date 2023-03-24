060328cf9a0afd511780f67fe195d34c0fb62877
mac <test>
2023-03-24 23:48:23 +0800

    remove Emoji

--- autumn-lib/src/main/java/cn/org/autumn/utils/Emoji.java
+++ autumn-lib/src/main/java/cn/org/autumn/utils/Emoji.java
@@ -3,34 +3,25 @@ package cn.org.autumn.utils;
 import org.apache.commons.lang3.StringUtils;
 
 public class Emoji {
-
-    private static boolean isEmoji(char codePoint) {
+    private static boolean isNotEmoji(char codePoint) {
         return codePoint == 0x0 || codePoint == 0x9 || codePoint == 0xA || codePoint == 0xD || codePoint >= 0x20 && codePoint <= 0xD7FF || codePoint >= 0xE000 && codePoint <= 0xFFFD;
     }
 
+    /**
+     * 过滤emoji 或者 其他非文字类型的字符
+     */
     public static String remove(String source) {
         if (StringUtils.isBlank(source)) {
-            return source;
+            return "";
         }
-        StringBuilder buf = null;
+        StringBuilder builder = new StringBuilder();
         int len = source.length();
         for (int i = 0; i < len; i++) {
             char codePoint = source.charAt(i);
-            if (isEmoji(codePoint)) {
-                if (buf == null) {
-                    buf = new StringBuilder(source.length());
-                }
-                buf.append(codePoint);
+            if (isNotEmoji(codePoint)) {
+                builder.append(codePoint);
             }
         }
-        if (buf == null) {
-            return source;
-        } else {
-            if (buf.length() == len) {
-                return source;
-            } else {
-                return buf.toString();
-            }
-        }
+        return builder.toString();
     }
 }
