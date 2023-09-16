f82ff202f8a20981e66adf257a31f80700dd0f64
shaohua <henryxm@163.com>
2023-09-16 21:20:41 +0800

    authenticated

---
+++ autumn-lib/src/main/java/cn/org/autumn/annotation/Authenticated.java
@@ -0,0 +1,12 @@
+package cn.org.autumn.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Authenticated {
+    boolean notNull() default true;
+}
