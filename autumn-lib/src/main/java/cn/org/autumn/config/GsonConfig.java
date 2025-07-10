0344db1af9b1a512a75aa30b428bd895a44559f0
mac <henryxm@163.com>
2025-07-10 17:23:01 +0800

    add default gson config

---
+++ autumn-lib/src/main/java/cn/org/autumn/config/GsonConfig.java
@@ -0,0 +1,14 @@
+package cn.org.autumn.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GsonConfig {
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+    }
+}
