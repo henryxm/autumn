376b7f3444d75d3ce063529ea8d839680952d06a
mac <henryxm@163.com>
2025-07-11 09:07:12 +0800

    log details

--- autumn-lib/src/main/java/cn/org/autumn/handler/AExceptionHandler.java
+++ autumn-lib/src/main/java/cn/org/autumn/handler/AExceptionHandler.java
@@ -3,57 +3,100 @@ package cn.org.autumn.handler;
 import cn.org.autumn.exception.AException;
 import cn.org.autumn.exception.CodeException;
 import cn.org.autumn.utils.R;
+import cn.org.autumn.utils.ExceptionUtils;
 import com.google.gson.Gson;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 异常处理器
  */
+@Slf4j
 @RestControllerAdvice
 public class AExceptionHandler {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
     /**
      * 处理自定义异常
      */
     @ExceptionHandler(AException.class)
-    public R handleRRException(AException e) {
-        if (logger.isDebugEnabled())
-            logger.debug("AException:{}", new Gson().toJson(e));
+    public R handleRRException(AException e, HttpServletRequest request) {
+        if (log.isDebugEnabled())
+            log.debug("AException:{}", new Gson().toJson(e));
         R r = new R();
         r.put("code", e.getCode());
         r.put("msg", e.getMessage());
-        logger.debug("AException", e);
+        log.debug("AException at {} {}", request.getMethod(), request.getRequestURI(), e);
         return r;
     }
 
     @ExceptionHandler(CodeException.class)
-    public R handleCodeException(CodeException e) {
-        if (logger.isDebugEnabled())
-            logger.debug("CodeException:{}", new Gson().toJson(e));
+    public R handleCodeException(CodeException e, HttpServletRequest request) {
+        if (log.isDebugEnabled())
+            log.debug("CodeException:{}", new Gson().toJson(e));
         R r = new R();
         r.put("code", e.getCode());
         r.put("msg", e.getMessage());
+        log.debug("CodeException at {} {}", request.getMethod(), request.getRequestURI(), e);
         return r;
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
-    public R handleDuplicateKeyException(DuplicateKeyException e) {
-        logger.debug("DuplicateKeyException", e);
+    public R handleDuplicateKeyException(DuplicateKeyException e, HttpServletRequest request) {
+        log.debug("DuplicateKeyException at {} {}", request.getMethod(), request.getRequestURI(), e);
         return R.error("数据库中已存在该记录");
     }
 
+    /**
+     * 处理媒体类型不可接受异常
+     */
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public R handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e, HttpServletRequest request) {
+        log.warn("HttpMediaTypeNotAcceptableException at {} {} - Accept: {}, Content-Type: {}",
+                request.getMethod(), request.getRequestURI(),
+                request.getHeader("Accept"), request.getHeader("Content-Type"), e);
+        return R.error(406, "请求的媒体类型不被支持");
+    }
+
+    /**
+     * 处理HTTP请求方法不支持异常
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public R handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
+        log.warn("HttpRequestMethodNotSupportedException at {} {} - Method: {}, Supported: {}",
+                request.getMethod(), request.getRequestURI(),
+                request.getMethod(), e.getSupportedMethods(), e);
+        return R.error(405, "请求方法不支持: " + request.getMethod());
+    }
+
+    /**
+     * 处理HTTP消息不可读异常
+     */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public R handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest request) {
+        log.warn("HttpMessageNotReadableException at {} {} - Content-Type: {}",
+                request.getMethod(), request.getRequestURI(),
+                request.getHeader("Content-Type"), e);
+        return R.error(400, "请求数据格式错误");
+    }
+
     @ExceptionHandler(Exception.class)
-    public R handleException(Exception e) {
-        //Suppress known exception log
-        if (!(e instanceof HttpMessageNotReadableException) && !(e instanceof HttpRequestMethodNotSupportedException))
-            logger.debug("Exception", e);
-        return R.error(e.getMessage());
+    public R handleException(Exception e, HttpServletRequest request) {
+        // 使用工具类记录详细的异常信息
+        ExceptionUtils.logDetailedException("未处理的异常", e, request);
+
+        // 如果是常见的HTTP异常，返回更友好的错误信息
+        if (ExceptionUtils.isCommonHttpException(e)) {
+            log.warn("常见HTTP异常: {}", ExceptionUtils.formatExceptionForLog(e, request));
+            return R.error("请求格式错误，请检查请求方法和内容类型");
+        }
+
+        // 返回通用错误信息，避免暴露敏感信息
+        return R.error("服务器内部错误，请联系管理员");
     }
 }
---
+++ autumn-lib/src/main/java/cn/org/autumn/utils/ExceptionUtils.java
@@ -0,0 +1,132 @@
+package cn.org.autumn.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 异常工具类
+ * 用于帮助调试和定位异常问题
+ */
+@Slf4j
+public class ExceptionUtils {
+
+    /**
+     * 获取异常的完整堆栈跟踪信息
+     */
+    public static String getStackTrace(Throwable throwable) {
+        if (throwable == null) {
+            return "";
+        }
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
+    }
+
+    /**
+     * 获取请求的详细信息
+     */
+    public static Map<String, Object> getRequestInfo(HttpServletRequest request) {
+        Map<String, Object> info = new HashMap<>();
+        if (request == null) {
+            return info;
+        }
+
+        info.put("method", request.getMethod());
+        info.put("uri", request.getRequestURI());
+        info.put("url", request.getRequestURL().toString());
+        info.put("queryString", request.getQueryString());
+        info.put("remoteAddr", request.getRemoteAddr());
+        info.put("remoteHost", request.getRemoteHost());
+        info.put("remotePort", request.getRemotePort());
+        info.put("userAgent", request.getHeader("User-Agent"));
+        info.put("accept", request.getHeader("Accept"));
+        info.put("contentType", request.getHeader("Content-Type"));
+        info.put("contentLength", request.getContentLength());
+
+        // 获取所有请求头
+        Map<String, String> headers = new HashMap<>();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            headers.put(headerName, headerValue);
+        }
+        info.put("headers", headers);
+
+        return info;
+    }
+
+    /**
+     * 记录详细的异常信息
+     */
+    public static void logDetailedException(String message, Exception e, HttpServletRequest request) {
+        if (log.isDebugEnabled()) {
+            log.error("=== 异常详细信息 ===");
+            log.error("消息: {}", message);
+            log.error("异常类型: {}", e.getClass().getName());
+            log.error("异常消息: {}", e.getMessage());
+
+            if (request != null) {
+                Map<String, Object> requestInfo = getRequestInfo(request);
+                log.error("请求信息: {}", requestInfo);
+            }
+
+            log.error("堆栈跟踪:");
+            log.error(getStackTrace(e));
+            log.error("=== 异常详细信息结束 ===");
+        }
+    }
+
+    /**
+     * 判断是否为常见的HTTP异常
+     */
+    public static boolean isCommonHttpException(Exception e) {
+        return e instanceof org.springframework.web.HttpMediaTypeNotAcceptableException ||
+                e instanceof org.springframework.web.HttpRequestMethodNotSupportedException ||
+                e instanceof org.springframework.http.converter.HttpMessageNotReadableException ||
+                e instanceof org.springframework.web.HttpMediaTypeNotSupportedException ||
+                e instanceof org.springframework.web.bind.MissingServletRequestParameterException;
+    }
+
+    /**
+     * 获取异常的根本原因
+     */
+    public static Throwable getRootCause(Throwable throwable) {
+        if (throwable == null) {
+            return null;
+        }
+
+        Throwable cause = throwable;
+        while (cause.getCause() != null && cause.getCause() != cause) {
+            cause = cause.getCause();
+        }
+        return cause;
+    }
+
+    /**
+     * 格式化异常信息用于日志输出
+     */
+    public static String formatExceptionForLog(Exception e, HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("异常类型: ").append(e.getClass().getSimpleName()).append("\n");
+        sb.append("异常消息: ").append(e.getMessage()).append("\n");
+
+        if (request != null) {
+            sb.append("请求方法: ").append(request.getMethod()).append("\n");
+            sb.append("请求URI: ").append(request.getRequestURI()).append("\n");
+            sb.append("客户端IP: ").append(request.getRemoteAddr()).append("\n");
+            sb.append("User-Agent: ").append(request.getHeader("User-Agent")).append("\n");
+        }
+
+        return sb.toString();
+    }
+} 
