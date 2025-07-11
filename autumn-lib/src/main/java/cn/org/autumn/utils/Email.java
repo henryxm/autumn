1038a3b44b0f75e6c4f018e83a6c48764fb28824
mac <henryxm@163.com>
2025-07-11 15:26:35 +0800

    fix

--- autumn-lib/src/main/java/cn/org/autumn/utils/Email.java
+++ autumn-lib/src/main/java/cn/org/autumn/utils/Email.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 
 public class Email {
     public static boolean isEmail(String email) {
+        if (null == email || email.length() < 3)
+            return false;
         String str = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
         Pattern p = Pattern.compile(str);
         Matcher m = p.matcher(email);
