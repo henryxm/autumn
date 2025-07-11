4fffe23589156e6bf74813f169dc72219c80136a
mac <henryxm@163.com>
2025-07-11 15:08:58 +0800

    log details

--- autumn-lib/src/main/java/cn/org/autumn/handler/AExceptionHandler.java
+++ autumn-lib/src/main/java/cn/org/autumn/handler/AExceptionHandler.java
@@ -48,7 +48,8 @@ public class AExceptionHandler {
 
     @ExceptionHandler(DuplicateKeyException.class)
     public R handleDuplicateKeyException(DuplicateKeyException e, HttpServletRequest request) {
-        log.debug("DuplicateKeyException at {} {}", request.getMethod(), request.getRequestURI(), e);
+        if (log.isDebugEnabled())
+            log.debug("DuplicateKeyException at {} {}", request.getMethod(), request.getRequestURI(), e);
         return R.error("数据库中已存在该记录");
     }
 
@@ -57,9 +58,10 @@ public class AExceptionHandler {
      */
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     public R handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e, HttpServletRequest request) {
-        log.warn("HttpMediaTypeNotAcceptableException at {} {} - Accept: {}, Content-Type: {}",
-                request.getMethod(), request.getRequestURI(),
-                request.getHeader("Accept"), request.getHeader("Content-Type"), e);
+        if (log.isDebugEnabled())
+            log.debug("HttpMediaTypeNotAcceptableException at {} {} - Accept: {}, Content-Type: {}",
+                    request.getMethod(), request.getRequestURI(),
+                    request.getHeader("Accept"), request.getHeader("Content-Type"), e);
         return R.error(406, "请求的媒体类型不被支持");
     }
 
@@ -68,9 +70,10 @@ public class AExceptionHandler {
      */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public R handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
-        log.warn("HttpRequestMethodNotSupportedException at {} {} - Method: {}, Supported: {}",
-                request.getMethod(), request.getRequestURI(),
-                request.getMethod(), e.getSupportedMethods(), e);
+        if (log.isDebugEnabled())
+            log.debug("HttpRequestMethodNotSupportedException at {} {} - Method: {}, Supported: {}",
+                    request.getMethod(), request.getRequestURI(),
+                    request.getMethod(), e.getSupportedMethods(), e);
         return R.error(405, "请求方法不支持: " + request.getMethod());
     }
 
@@ -79,9 +82,10 @@ public class AExceptionHandler {
      */
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public R handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest request) {
-        log.warn("HttpMessageNotReadableException at {} {} - Content-Type: {}",
-                request.getMethod(), request.getRequestURI(),
-                request.getHeader("Content-Type"), e);
+        if (log.isDebugEnabled())
+            log.debug("HttpMessageNotReadableException at {} {} - Content-Type: {}",
+                    request.getMethod(), request.getRequestURI(),
+                    request.getHeader("Content-Type"), e);
         return R.error(400, "请求数据格式错误");
     }
 
@@ -89,10 +93,10 @@ public class AExceptionHandler {
     public R handleException(Exception e, HttpServletRequest request) {
         // 使用工具类记录详细的异常信息
         ExceptionUtils.logDetailedException("未处理的异常", e, request);
-
         // 如果是常见的HTTP异常，返回更友好的错误信息
         if (ExceptionUtils.isCommonHttpException(e)) {
-            log.warn("常见HTTP异常: {}", ExceptionUtils.formatExceptionForLog(e, request));
+            if (log.isDebugEnabled())
+                log.debug("常见HTTP异常: {}", ExceptionUtils.formatExceptionForLog(e, request));
             return R.error("请求格式错误，请检查请求方法和内容类型");
         }
 
