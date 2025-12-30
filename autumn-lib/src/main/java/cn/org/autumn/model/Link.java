715678651843bceb0e668af20d33df0ba1ef2916
shaohua <henryxm@163.com>
2025-12-30 15:17:51 +0800

    add link model

--- autumn-lib/src/main/java/cn/org/autumn/model/Link.java
+++ autumn-lib/src/main/java/cn/org/autumn/model/Link.java
@@ -1,6 +1,7 @@
 package cn.org.autumn.model;
 
 import cn.org.autumn.annotation.JsonMap;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,13 +16,20 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Schema(name = "链接")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Link implements Serializable {
 
     @Schema(name = "地址")
-    private String url = "";
+    private String url;
 
     @Schema(name = "标题")
-    private String title = "";
+    private String icon;
+
+    @Schema(name = "标题")
+    private String title;
+
+    @Schema(name = "子标题")
+    private String subtitle;
 
     public Link(String url) {
         this.url = url;
